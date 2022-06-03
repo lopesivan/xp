82092c1a6c00f3507ee39b38b950f97b7c0a4755
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-03 04:46:39 -0300

    Atualiza o arquivo tasks/ls.sh

--- tasks/ls.sh
+++ tasks/ls.sh
@@ -11,13 +11,15 @@ function btask.ls.run() {
 
 	if [ $# -eq 1 ]; then
 
-
 		if redis-cli TYPE $1 | grep -q 'zset'; then
+            echo "1111"
 			redis-cli ZRANGE $1 0 -1
 		else
             if [[ "$1" =~ 's-*' ]]; then
+            echo "2222"
                 redis-cli LRANGE $1 0 -1
             else
+            echo "3333"
 			    redis-cli get $1 |
 				    sed "s/^/$(tput bold) /"
             fi
