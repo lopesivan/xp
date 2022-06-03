adb0ecafbd048b06c426f441cf20ce02b59cb6ef
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-03 03:31:49 -0300

    Adiciona o arquivo tasks/push.sh

---
+++ tasks/push.sh
@@ -0,0 +1,30 @@
+# tasks/push.sh
+function btask.push.run() {
+	if [ "$1" == '--help' ]; then
+		b.get "xp.config.help_message_push"
+
+		return $(b.get "xp.config.help_message")
+	fi
+
+	source ${HOME}/developer/redis-bash/redis-bash-lib # include the library
+	exec 6<>/dev/tcp/localhost/6379                    # open the connection
+
+	if [ "$1" == '-n' ]; then
+		local name=$2
+		echo : $name :
+		echo : ${LOCALPATH} :
+		cat - |
+			while read f; do
+				redis-client 6 RPUSH s-$name $f
+				# echo $f
+			done | wc -l | sed 's/^.*/add: & lines/'
+
+	else
+		redis-client 6 RPUSH s-$name $1
+	fi
+
+	exec 6>&- # close the connection
+	return $?
+}
+
+# vim: set ts=4 sw=4 tw=78 ft=sh:
