59dd366ceaf0ae305a86e24610ed1f520c03685a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 22:04:43 -0300

    Atualiza o arquivo etc/xpdoc.json

---
+++ tasks/cat.sh
@@ -0,0 +1,35 @@
+# tasks/cat.sh
+function btask.cat.run() {
+	if [ "$1" == '--help' ]; then
+		b.get "xp.config.help_message_cat"
+
+		return $(b.get "xp.config.help_message")
+	fi
+
+    if [ -n "$1" ]; then
+        source ${HOME}/developer/redis-bash/redis-bash-lib # include the library
+        exec 6<>/dev/tcp/localhost/6379                    # open the connection
+        # isOK
+        redis-cli TYPE $1| grep -q 'zset'
+        if [ $? -eq 0 ]; then
+            if [ $# -eq 1 ]; then
+                file=$( redis-cli ZRANGE $1 0 -1 |
+                    fzf)
+            else
+                file=$( redis-cli ZRANGE $1 0 -1 |
+                    fzf )
+            fi
+            [ -n "$file" ] && {
+                vim $file
+            }
+        else
+            redis-cli get $1 | xargs cat
+        fi
+        # close the connection
+        exec 6>&-
+    fi
+
+	return $?
+}
+
+# vim: set ts=4 sw=4 tw=78 ft=sh:
