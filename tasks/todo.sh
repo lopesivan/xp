abb826d03c8b2ee191958a1847c16cb030b421eb
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-07 15:07:08 -0300

    Removendo i3 do task/todo

--- tasks/todo.sh
+++ tasks/todo.sh
@@ -6,9 +6,7 @@ function btask.todo.run() {
         return $( b.get "xp.config.help_message" )
     fi
 
-    local exe="exec --no-startup-id"
-    local TERMINAL=st
-	i3 "$exe $TERMINAL -n wldoc -e sh -c ${HOME}/developer/scripts/show-edit-ldoc/docs1.sh"
+	sh -c "${HOME}/developer/scripts/show-edit-ldoc/docs1.sh"
 }
 
 # vim: set ts=4 sw=4 tw=78 ft=sh:
